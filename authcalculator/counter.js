bba767e0b161d686de248aa4c83431df68c107fe
Yeoh-TH <soundwavedecepticonleader@gmail.com>
2023-11-22 10:57:12 +0800

    Email

--- authcalculator/counter.js
+++ authcalculator/counter.js
@@ -1,3 +1,42 @@
+import { initializeApp } from "firebase/app";
+import {
+    GoogleAuthProvider,
+    getAuth,
+    onAuthStateChanged,
+    signInWithPopup,
+    RecaptchaVerifier
+} from "firebase/auth";
+// export let user = "";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyJkWTGjQuOed4AVSyQtyLyXYm0efLUKQ",
+  authDomain: "authcalculator.firebaseapp.com",
+  projectId: "authcalculator",
+  storageBucket: "authcalculator.appspot.com",
+  messagingSenderId: "546894117488",
+  appId: "1:546894117488:web:7bd4612fe2741260dbd2b5",
+  measurementId: "G-V133K4J603"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+var user = auth.currentUser;
+window.recaptchaVerifier = new RecaptchaVerifier(auth, 'recaptcha-container', {});
+
+    let userLogin = () => {
+        signInWithPopup(auth, new GoogleAuthProvider()).then((result) => {
+            user = result.user;
+            window.location = "#/";
+        });
+    };
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+          console.log("You are logged in as", user);
+          loadSaves();
+      }
+  });
+
 export function setupCounter(element) {
   let counter = 0;
 
@@ -14,23 +53,14 @@ export function setupCounter(element) {
 
   let currentLeft = parseInt(element.style.left);
   let currentRight = parseInt(element.style.right);
-  element.style.left = currentLeft + getRandomInt(1000) + 'px';
-  element.style.right = currentRight + getRandomInt(1000) + 'px';
-
-
-
-
-  if (currentLeft > currentRight) {
-    element.style.right = currentLeft + getRandomInt(1000) + 'px';
-  }
-  else{
-    element.style.left = currentRight + getRandomInt(1000) + 'px';
-  }
-  }
+  element.style.left = currentLeft + getRandomInt(100) + 'vw';
+  element.style.right = currentRight + getRandomInt(100) + 'vw';
+}
 
   element.addEventListener('click', () => {
     setCounter(counter + 1);
     moveItem(); 
+    userLogin();
   });
 
   setCounter(0);
--- authcalculator/main.js
+++ authcalculator/main.js
@@ -1,11 +1,48 @@
 import './style.css'
 import { setupCounter } from './counter.js'
+import { initializeApp } from "firebase/app";
+import {
+    GoogleAuthProvider,
+    getAuth,
+    onAuthStateChanged,
+    signInWithPopup,
+    RecaptchaVerifier
+} from "firebase/auth";
+// export let user = "";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyJkWTGjQuOed4AVSyQtyLyXYm0efLUKQ",
+  authDomain: "authcalculator.firebaseapp.com",
+  projectId: "authcalculator",
+  storageBucket: "authcalculator.appspot.com",
+  messagingSenderId: "546894117488",
+  appId: "1:546894117488:web:7bd4612fe2741260dbd2b5",
+  measurementId: "G-V133K4J603"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+var user = auth.currentUser;
+
+    let userLogin = () => {
+        signInWithPopup(auth, new GoogleAuthProvider()).then((result) => {
+            user = result.user;
+            window.location = "#/";
+        });
+    };
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+          console.log("You are logged in as", user);
+          loadSaves();
+      }
+  });
+
 
 document.querySelector('#app').innerHTML = `
-  <div>
+  <div >
     <h1>Calculator go brrrrr</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="counter" type="button" onClick={() => userLogin()}></button>
     </div>
   </div>
 `
